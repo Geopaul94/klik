c6d97233c9c2c21186db3166e70a20ff7fb37845
geopaul94 <geopaul94@gmail.com>
2024-11-06 19:51:54 +0530

    latest save error .

--- lib/presentaion/pages/homepage/homepage.dart
+++ lib/presentaion/pages/homepage/homepage.dart
@@ -386,8 +386,47 @@ class _HomPage_cardState extends State<HomPage_card> {
             ),
           ],
         ),
-        saveIcon(context),
+       // saveIcon(context),
 
+
+
+
+
+BlocBuilder<SaveUnsaveBloc, SaveUnsaveState>(
+  builder: (context, state) {
+    // Determine if the post is already saved based on the current Bloc state
+    bool isAlreadySaved = posts.any(
+      (element) => element.postId.id == widget.HomePagePosts.id,
+    );
+
+    if (state is SavePostSuccessfullState && state.post == widget.HomePagePosts.id) {
+      isAlreadySaved = true;
+    } else if (state is RemoveSavedPostSuccessfulState && state.post == widget.HomePagePosts.id) {
+      isAlreadySaved = false;
+    }
+
+    return IconButton(
+      onPressed: () async {
+        if (isAlreadySaved) {
+          // Dispatch the unsave event
+          context.read<SaveUnsaveBloc>().add(OnUserRemoveSavedPost(
+                postId: widget.HomePagePosts.id.toString(),
+              ));
+        } else {
+          // Dispatch the save event
+          context.read<SaveUnsaveBloc>().add(OnUserSavePost(
+                postId: widget.HomePagePosts.id.toString(),
+              ));
+        }
+      },
+      icon: Icon(
+        isAlreadySaved ? CupertinoIcons.bookmark_fill : CupertinoIcons.bookmark,
+        color: blue,
+        size: 25,
+      ),
+    );
+  },
+)
 
 
 
@@ -402,63 +441,70 @@ class _HomPage_cardState extends State<HomPage_card> {
    // );
   }
 
-  IconButton saveIcon(BuildContext context) {
-    return IconButton(
-      onPressed: () async {
-        bool isAlreadySaved = posts.any(
-          (element) => element.postId.id == widget.HomePagePosts.id,
-        );
+  // IconButton saveIcon(BuildContext context) {
+  //   return IconButton(
+  //     onPressed: () async {
+  //       bool isAlreadySaved = posts.any(
+  //         (element) => element.postId.id == widget.HomePagePosts.id,
+  //       );
 
-        if (isAlreadySaved) {
-          context.read<SaveUnsaveBloc>().add(OnUserRemoveSavedPost(
-                postId: widget.HomePagePosts.id.toString(),
-              ));
+  //       if (isAlreadySaved) {
+  //         context.read<SaveUnsaveBloc>().add(OnUserRemoveSavedPost(
+  //               postId: widget.HomePagePosts.id.toString(),
+  //             ));
 
-          posts.removeWhere(
-            (element) => element.postId.id == widget.HomePagePosts.id,
-          );
+  //         posts.removeWhere(
+  //           (element) => element.postId.id == widget.HomePagePosts.id,
+  //         );
 
-          print("Post removed: ${widget.HomePagePosts.id}");
-        } else {
-          // If the post is not saved, add it to the saved list
-          posts.add(SavedPostModel(
-            userId: widget.HomePagePosts.userId.id.toString(),
-            postId: PostId(
-              id: widget.HomePagePosts.id.toString(),
-              userId: UserIdSavedPost.fromJson(
-                  widget.HomePagePosts.userId.toJson()),
-              image: widget.HomePagePosts.image.toString(),
-              description: widget.HomePagePosts.description.toString(),
-              likes: widget.HomePagePosts.likes,
-              hidden: widget.HomePagePosts.hidden,
-              blocked: widget.HomePagePosts.blocked,
-              tags: widget.HomePagePosts.tags,
-              date: widget.HomePagePosts.createdAt,
-              createdAt: widget.HomePagePosts.createdAt,
-              updatedAt: widget.HomePagePosts.updatedAt,
-              v: widget.HomePagePosts.v,
-              taggedUsers: widget.HomePagePosts.taggedUsers,
-            ),
-            createdAt: DateTime.now(),
-            updatedAt: DateTime.now(),
-            v: widget.HomePagePosts.v,
-          ));
+  //         print("Post removed: ${widget.HomePagePosts.id}");
+  //       } else {
+  //         // If the post is not saved, add it to the saved list
+  //         posts.add(SavedPostModel(
+  //           userId: widget.HomePagePosts.userId.id.toString(),
+  //           postId: PostId(
+  //             id: widget.HomePagePosts.id.toString(),
+  //             userId: UserIdSavedPost.fromJson(
+  //                 widget.HomePagePosts.userId.toJson()),
+  //             image: widget.HomePagePosts.image.toString(),
+  //             description: widget.HomePagePosts.description.toString(),
+  //             likes: widget.HomePagePosts.likes,
+  //             hidden: widget.HomePagePosts.hidden,
+  //             blocked: widget.HomePagePosts.blocked,
+  //             tags: widget.HomePagePosts.tags,
+  //             date: widget.HomePagePosts.createdAt,
+  //             createdAt: widget.HomePagePosts.createdAt,
+  //             updatedAt: widget.HomePagePosts.updatedAt,
+  //             v: widget.HomePagePosts.v,
+  //             taggedUsers: widget.HomePagePosts.taggedUsers,
+  //           ),
+  //           createdAt: DateTime.now(),
+  //           updatedAt: DateTime.now(),
+  //           v: widget.HomePagePosts.v,
+  //         ));
 
-          // Add the post to the backend
-          context.read<SaveUnsaveBloc>().add(OnUserSavePost(
-                postId: widget.HomePagePosts.id.toString(),
-              ));
-        }
-      },
-      icon: Icon(
-        posts.any((element) => element.postId.id == widget.HomePagePosts.id)
-            ? CupertinoIcons.bookmark_fill
-            : CupertinoIcons.bookmark,
-        color: blue,
-        size: 25,
-      ),
-    );
-  }
+  //         // Add the post to the backend
+  //         context.read<SaveUnsaveBloc>().add(OnUserSavePost(
+  //               postId: widget.HomePagePosts.id.toString(),
+  //             ));
+  //       }
+  //     },
+  //     icon: Icon(
+  //       posts.any((element) => element.postId.id == widget.HomePagePosts.id)
+  //           ? CupertinoIcons.bookmark_fill
+  //           : CupertinoIcons.bookmark,
+  //       color: blue,
+  //       size: 25,
+  //     ),
+  //   );
+  // }
+
+
+
+
+
+
+
 
   Row nameAndDateRow() {
     return Row(
